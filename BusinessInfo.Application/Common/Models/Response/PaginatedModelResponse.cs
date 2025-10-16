708bf652f182d132c81f005603bd25459b38ef07
Thiago Santos <thiago.santos@okta7.com.br>
2025-10-16 19:55:00 -0300

    feature/list-vehicles: Api de Listagem de Veiculos #5

---
+++ BusinessInfo.Application/Common/Models/Response/PaginatedModelResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessInfo.Application.Common.Models.Response
+{
+    public class PaginatedModelResponse<T>
+    {
+        public int Total { get; set; }
+        public IEnumerable<T> Items { get; set; }
+        protected PaginatedModelResponse() { }
+        public PaginatedModelResponse(int total, IEnumerable<T> items)
+        {
+            Total = total;
+            Items = items;
+        }
+    }
+}
